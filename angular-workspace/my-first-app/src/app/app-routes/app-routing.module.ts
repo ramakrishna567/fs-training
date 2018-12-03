0807f67d1e5b38e6b6c4c67d9688f8757da08a02
Ram <ram567@gmail.com>
2018-12-03 11:07:05 +0530

    created ROUTING n components

---
+++ angular-workspace/my-first-app/src/app/app-routes/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule,Routes } from '@angular/router';
+import { CarouselComponent } from '../home/carousel/carousel.component';
+import { HomeComponent } from '../home/home.component';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+
+const routes: Routes = [
+    //For empty URL
+    { path: "", component: HomeComponent },
+    { path: "home", component: HomeComponent },
+    { path: "login", component: LoginComponent },
+    { path: "register", component: RegisterComponent },
+    //not Found Url for any url
+    { path: "**", component: NotFoundComponent }
+]
+//This if group components
+@NgModule({
+    imports: [],
+    declarations: [],
+    exports: []
+})
--- angular-workspace/my-first-app/src/app/app.module.ts
+++ angular-workspace/my-first-app/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    HeaderComponent,
+    FooterComponent,
+    CarouselComponent,
+    NavigationComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
